0414cf0bdedee182e17b55540ba8b995a0766799
Jerrold Thompson <jet@whidbey.com>
2014-12-24 05:35:47 -0800

    Chap 6 Code

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -23,16 +23,21 @@ class QuotesController < Rulers::Controller
     raise "It's a bad one!"
   end
 
+  def show
+    quote = FileModel.find(params["id"]) 
+    ua = request.user_agent
+    render_response  :quote, obj: quote, ua: ua 
+  end
+
   def  index
     quotes = FileModel.all
     render :index, quotes: quotes
   end
 
-  def submitted_by(params)
-    require 'pry'; binding.pry
+  def submitted_by
     @author = []
-    @author = FileModel.find_all_by_submitter(params["author"])
     require 'pry'; binding.pry
+    @author = FileModel.find_all_by_submitter(params["author"])
     if @author.empty?
       render :submitted_by, author: ( @author << "none" )
     else
@@ -40,10 +45,9 @@ class QuotesController < Rulers::Controller
     end
   end
 
-  def update(params)
+  def update
     @hash = {}
     cur_quote = FileModel.find(params["id"])
-    binding.pry
     @hash["submitter"] = params["submitter"] || cur_quote["submitter"]
     @hash["quote"] = params["quote"] || cur_quote["quote"]
     @hash["attribution"] = params["attribution"] || cur_quote["attribution"]
